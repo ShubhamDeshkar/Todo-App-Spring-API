bb738302859523bc39e9a0add17947b12edd97fc
Shubham Deshkar <shubham_deshkar@yahoo.com>
2019-06-20 10:09:40 -0400

    configure AuthenticationController

--- src/main/java/com/todoapp/TodoApp/basic/auth/AuthenticationController.java
+++ src/main/java/com/todoapp/TodoApp/basic/auth/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "https://quirky-morse-bbfee4.netlify.com"})
 public class AuthenticationController {
     @GetMapping("/basic-auth")
     public AuthenticatedBean helloWorldBean() {
