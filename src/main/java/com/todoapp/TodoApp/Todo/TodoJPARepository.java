1150b306a66c7feb1fdbf80d3f8f98561989f9d6
Shubham Deshkar <shubham_deshkar@yahoo.com>
2019-07-02 11:39:14 -0400

    declare @Repository

--- src/main/java/com/todoapp/TodoApp/Todo/TodoJPARepository.java
+++ src/main/java/com/todoapp/TodoApp/Todo/TodoJPARepository.java
@@ -1,9 +1,11 @@
 package com.todoapp.TodoApp.Todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TodoJPARepository extends JpaRepository<Todo, Long>{
     public List<Todo> findByUsername(String username);
 }
