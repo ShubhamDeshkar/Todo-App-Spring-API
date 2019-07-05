c80eca5cf3b2a44ca86a4fe6a5d87dd656c920da
Shubham Deshkar <shubham_deshkar@yahoo.com>
2019-07-05 17:47:53 -0400

    make User @RequestBody in arg

--- src/main/java/com/todoapp/TodoApp/users/UserJPAResource.java
+++ src/main/java/com/todoapp/TodoApp/users/UserJPAResource.java
@@ -2,10 +2,7 @@ package com.todoapp.TodoApp.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -19,11 +16,9 @@ public class UserJPAResource {
     private UserJPARepository userJPARepository;
 
     @PostMapping("/new")
-    public ResponseEntity<Void> createUser(User newUser) {
+    public ResponseEntity<Void> createUser(@RequestBody User newUser) {
         User createdUser = userJPARepository.save(newUser);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
-
-
 }
